28e10506ac3f68d8c786e486c258731ce8704730
Joseph Haaga <josephhaaga@Josephs-MacBook-Pro.local>
2018-12-10 12:15:07 -0500

    dropdown to change Vertex type

--- src/Graph.js
+++ src/Graph.js
@@ -7,43 +7,79 @@ class Graph extends Component {
     this.addNode = this.addNode.bind(this)
     this.state = {
       nodes: [
-        {id: 'vader', label: 'Darth Vader'},
-        {id: 'luke', label: 'Luke Skywalker'},
-        {id: 'leia', label: 'Leia Organa'},
+        {id: 'v1', type: 'Person'},
+        {id: 'v2', type: 'Vertex'},
+        {id: 'v3', type: 'Person'},
       ],
       edges: [
-        {id: '1', from: 'vader', to: 'luke'},
-        {id: '2', from: 'vader', to: 'leia'},
-      ]
+        {id: 'e1', from: 'v1', to: 'v2', type: 'employed_by'},
+        {id: 'e2', from: 'v3', to: 'v2', type: 'Edge'},
+      ],
+      nodeTypes: ["Vertex", "Person", "Company"],
+      edgeTypes: ["Edge", "employed_by", "claims_dependent"]
     }
   }
 
   addNode(){
     let n = this.state.nodes
-    const newId = "vertex"+n.length
-    n.push({id: newId, label:'New Node'})
+    const newId = "v" + (n.length + 1)
+    n.push({id: newId, type: 'Vertex'})
     this.setState({nodes: n})
   }
 
+  addEdge(){
+    let nodes = this.state.nodes
+    let e = this.state.edges
+    // const newId = "vertex"+n.length
+    // n.push({id: newId, label:'New Node'})
+    // this.setState({edges: n})
+  }
+
+  updateType(id, newValue){
+    if(id[0] === "e"){
+      let e = this.state.edges
+      const indexToUpdate = e.findIndex((z)=>z.id == id)
+      e[indexToUpdate]['type'] = newValue
+      this.setState({edges: e})
+    }else{
+      let n = this.state.nodes
+      const indexToUpdate = n.findIndex((z)=>z.id == id)
+      n[indexToUpdate]['type'] = newValue
+      this.setState({nodes: n})
+    }
+  }
+
   render(){
-    const Decorator = props => {
+    const NodeDecorator = props => {
       return (
-        <button onClick={(e) => console.log(`You clicked ${props.label}`)}>
-          Click Me
-        </button>
+        <select value={props.type} onChange={(e) => this.updateType(props.id, e.target.value)}>
+          {this.state.nodeTypes.map(x =>
+            <option value={x} key={"node-"+x}>{x}</option>
+          )}
+        </select>
+      );
+    };
+    const EdgeDecorator = props => {
+      return (
+        <select value={props.type} onChange={(e) => this.updateType(props.id, e.target.value)}>
+          {this.state.edgeTypes.map(x =>
+            <option value={x} key={"edge-"+x}>{x}</option>
+          )}
+        </select>
       );
     };
 
     const nodes = this.state.nodes.map(x =>
-      <Node key={"n"+x.id} id={x.id} label={x.label}  decorator={Decorator} />
+      <Node key={"n"+x.id} id={x.id} label={x.id+": "+x.type} type={x.type} decorator={NodeDecorator} />
     )
     const edges = this.state.edges.map(x =>
-      <Edge key={"e"+x.id} id={x.id} from={x.from} to={x.to} />
+      <Edge key={"e"+x.id} id={x.id} from={x.from} to={x.to} label={x.type} decorator={EdgeDecorator} type={x.type}/>
     )
     return (
       <div>
         <button onClick={this.addNode}>Add Node</button>
-        <Network>
+        <button onClick={this.addEdge}>Add Edge</button>
+        <Network height={600}>
           {nodes}
           {edges}
         </Network>
